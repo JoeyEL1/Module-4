72d1731fc74a42963e739079a6e8f967d08ebd91
jobatkinIOD <jo.batkin@institutedata.com>
2022-11-03 15:08:09 +1000

    reviewed, suggested changes

--- Slide 38/script.js
+++ Slide 38/script.js
@@ -3,7 +3,7 @@ let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
     if(e.target.innerHTML == '='){
-      string = eval(string);
+      string = eval(string); //try to avoid using the eval function as it's seen as a security risk and will red-flag your code
       document.querySelector('input').value = string;
     }
     else if(e.target.innerHTML == 'C'){
